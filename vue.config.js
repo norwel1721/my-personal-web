cb16f2418ff8952b87709df4a592f01ae6abfddb
norwel <norwel1217@gmail.com>
2023-08-29 02:13:11 +0800

    'updatess'

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,11 @@ module.exports = {
         .options({
           name: 'assets/[name].[hash:8].[ext]'
         });
+
+        
     },
+
+    publicPath: process.env.NODE_ENV === "production" ? "/my-personal-web/" : "/",
 
     devServer: {
       host: '10.169.141.202',
